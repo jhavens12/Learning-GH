c4e50cef77d427a1fa397e45178c3c81bff05175
Jonathan Havens <jhavens12@gmail.com>
2021-07-01 10:25:20 -0400

    Initial Commit

---
+++ Learning-GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning-GH
+//
+//  Created by Jonathan Havens on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
